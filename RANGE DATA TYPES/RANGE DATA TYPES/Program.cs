402fea1704e7504156a1db90db06d8a863f6d857
Anas-Electro <anas.electro212@gmail.com>
2024-06-10 18:52:20 +0100

    RANGE DATA TYPES IN CODE VISUAL STUDIO

--- RANGE DATA TYPES/RANGE DATA TYPES/Program.cs
+++ RANGE DATA TYPES/RANGE DATA TYPES/Program.cs
@@ -10,6 +10,58 @@ namespace RANGE_DATA_TYPES
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("byte");
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            // byte nbr = 256;
+            Console.WriteLine("==============");
+
+            Console.WriteLine("sbyte");
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("short");
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine("==============");
+
+            Console.WriteLine("ushort");
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+
+            Console.WriteLine("==============");
+            Console.WriteLine("int");
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            Console.WriteLine("==============");
+            Console.WriteLine("uint");
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+
+            Console.WriteLine("==============");
+            Console.WriteLine("long");
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            Console.WriteLine("==============");
+            Console.WriteLine("float");
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            Console.WriteLine("==============");
+            Console.WriteLine("decimal");
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+            Console.WriteLine("==============");
+            Console.WriteLine("char");
+            Console.WriteLine(char.MinValue);
+            Console.WriteLine(char.MaxValue);
+
+
+
         }
     }
 }
