e1c52af1129df1b6bd537b9cdd17185de188c593
Anas-Electro <anas.electro212@gmail.com>
2024-06-10 19:06:03 +0100

    C# SIZE DATA TYPES IN CODE VISUAL STUDIO

--- RANGE DATA TYPES/RANGE DATA TYPES/Program.cs
+++ RANGE DATA TYPES/RANGE DATA TYPES/Program.cs
@@ -13,59 +13,73 @@ namespace RANGE_DATA_TYPES
             Console.WriteLine("byte");
             Console.WriteLine(byte.MinValue);
             Console.WriteLine(byte.MaxValue);
+            Console.Write("a size of a byte is ");
+            Console.WriteLine(sizeof(byte));
             // byte nbr = 256;
             Console.WriteLine("==============");
 
             Console.WriteLine("sbyte");
             Console.WriteLine(sbyte.MinValue);
             Console.WriteLine(sbyte.MaxValue);
+            Console.Write("a size of a sbyte is ");
+            Console.WriteLine(sizeof(sbyte));
             Console.WriteLine("==============");
 
             Console.WriteLine("short");
             Console.WriteLine(short.MinValue);
             Console.WriteLine(short.MaxValue);
+            Console.Write("a size of a short is ");
+            Console.WriteLine(sizeof(short));
             Console.WriteLine("==============");
 
             Console.WriteLine("ushort");
             Console.WriteLine(ushort.MinValue);
             Console.WriteLine(ushort.MaxValue);
-
+            Console.Write("a size of a ushort is ");
+            Console.WriteLine(sizeof(ushort));
             Console.WriteLine("==============");
             Console.WriteLine("int");
             Console.WriteLine(int.MinValue);
             Console.WriteLine(int.MaxValue);
-
+            Console.Write("a size of a int is ");
+            Console.WriteLine(sizeof(int));
             Console.WriteLine("==============");
             Console.WriteLine("uint");
             Console.WriteLine(uint.MinValue);
             Console.WriteLine(uint.MaxValue);
-
+            Console.Write("a size of a uint is ");
+            Console.WriteLine(sizeof(uint));
             Console.WriteLine("==============");
             Console.WriteLine("long");
             Console.WriteLine(long.MinValue);
             Console.WriteLine(long.MaxValue);
-
+            Console.Write("a size of a long is ");
+            Console.WriteLine(sizeof(long));
             Console.WriteLine("==============");
             Console.WriteLine("float");
             Console.WriteLine(float.MinValue);
             Console.WriteLine(float.MaxValue);
-
+            Console.Write("a size of a float is ");
+            Console.WriteLine(sizeof(float));
             Console.WriteLine("==============");
             Console.WriteLine("double");
             Console.WriteLine(double.MinValue);
             Console.WriteLine(double.MaxValue);
-
+            Console.Write("a size of a double is ");
+            Console.WriteLine(sizeof(double));
 
             Console.WriteLine("==============");
             Console.WriteLine("decimal");
             Console.WriteLine(decimal.MinValue);
             Console.WriteLine(decimal.MaxValue);
-
+            Console.Write("a size of a decimal is ");
+            Console.WriteLine(sizeof(decimal));
             Console.WriteLine("==============");
             Console.WriteLine("char");
             Console.WriteLine(char.MinValue);
             Console.WriteLine(char.MaxValue);
-
+            Console.Write("a size of a char is ");
+            Console.WriteLine(sizeof(char));
 
 
         }
