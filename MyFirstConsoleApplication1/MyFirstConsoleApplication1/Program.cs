7efd2873f1ac5117f484146d147adcd8cb4501ee
Anas-Electro <anas.electro212@gmail.com>
2024-06-10 17:30:28 +0100

    C# LEARNING

---
+++ MyFirstConsoleApplication1/MyFirstConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // write a number using write
+            // بالنسبة فهي تطبع فقط بدون رجوع للسطر   
+            Console.Write(3500);
+            // write a number using  writeline
+            // فهي تطبع وترجع للسطر  
+            Console.WriteLine(3500);
+            // write a text using writeline "Welcome to egypte"
+            // طبع جملة مع الرجوع الى سطر
+            Console.WriteLine("Welcome to egypte");     
+        }
+    }
+}
