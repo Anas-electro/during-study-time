4097e578267f77d4615083e66ff208f3bcaa39eb
Anas-Electro <anas.electro212@gmail.com>
2024-06-11 10:01:45 +0100

    Update

---
+++ 6 - TestAboutChar/TestAboutChar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAboutChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1 = 'A';
+            Console.WriteLine(c1);
+        }
+    }
+}
