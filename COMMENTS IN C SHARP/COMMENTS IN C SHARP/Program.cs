b56cf89d459f85832aa0d452ec2d5cb3abc7947d
Anas-Electro <anas.electro212@gmail.com>
2024-06-12 16:55:43 +0100

    C# COMMENTS IN C SHARP

---
+++ COMMENTS IN C SHARP/COMMENTS IN C SHARP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMMENTS_IN_C_SHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Comment1
+            //Comment2
+            //Comment3
+
+            /*
+             Comment1
+             Comment2
+             Comment3
+             Comment4
+             AllComment
+               */
+
+            // هذا السطر لتعريف متغير الاسم
+            // This line for declare variable of name
+            string strName;
+
+            strName = "Anas";  // متغير لوضع القيمة
+
+            // print name
+            Console.WriteLine(strName);
+
+            //////int nbr1;
+            //////nbr1 = 5;
+            //////int nbr2;
+            //////nbr2 = 10;
+            //////Console.WriteLine(nbr1);
+            //////Console.WriteLine(nbr2);
+
+
+
+        }
+    }
+}
