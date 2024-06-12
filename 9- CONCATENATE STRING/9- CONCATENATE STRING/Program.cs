f0678bf1db999e5997ef22f0388af380e3dbd734
Anas-Electro <anas.electro212@gmail.com>
2024-06-12 18:21:01 +0100

    C# CONCATENATE STRING IN C

---
+++ 9- CONCATENATE STRING/9- CONCATENATE STRING/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9__CONCATENATE_STRING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str1 = "Hello";
+            //string str2 = " ";
+            //string str3 = "Anas";
+            //string strAll = str1 + str2 + str3;
+
+            ////Console.WriteLine(str1 + str2 + str3);
+            //Console.WriteLine(strAll);
+            // Console.WriteLine("Welcome" + " " + "To" + " " + "Egypte");
+
+            //string strNames = "Ahmed" 
+            //    + "," 
+            //    + "Amr" 
+            //    + "," 
+            //    + "Ali"
+            //    + ","
+            //    + "Omar" + "," + "Kareem";
+
+            //Console.WriteLine(strNames);
+
+            //string strName = "Hamed";
+            //int num1 = 123;
+
+            //char c = 'Z';
+            //string strAll2 = strName + " " + num1 + " " + c;
+            //// Console.WriteLine(strName + " " + num1 + " " + c);
+            //Console.WriteLine(strAll2);
+
+            string strName;
+            strName = "Welcome" + " " + "Ahmed";
+            Console.WriteLine(strName);
+
+
+        }
+    }
+}
