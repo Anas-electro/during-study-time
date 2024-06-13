4da52a4366fd0580d9cca9e83c041912d8ec361c
Anas-Electro <anas.electro212@gmail.com>
2024-06-13 16:54:55 +0100

    C# ASSIGNMENT OPERATORS

---
+++ 11 - ASSIGNMENT OPERATORS/11 - ASSIGNMENT OPERATORS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11___ASSIGNMENT_OPERATORS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// =
+           // int x = 5;
+
+            /////// +=  ,  -=  ,  *=  , /=  ,  %=
+
+            //int nbr;
+            //nbr = 5;
+            //nbr += 10; // nbr = nbr + 10; 15 = 5 + 10;
+            //nbr += 2; // nbr = nbr + 2;  17 = 2 + 15;
+            //nbr -= 3; // nbr = nbr - 3;  14 = 17 - 3;
+            //nbr *= 2; // nbr = nbr * 2;  28 = 14 * 2;
+            //nbr /= 3; // nbr = nbr / 3;  9 = 28 / 3;
+            //nbr %= 2; // nbr = nbr % 2;  1 = 9 % 2;
+            //nbr += 22; // nbr = nbr + 22; 23 = 22 + 1;
+            //Console.WriteLine(nbr);
+
+            //Console.WriteLine("Hello" + " " + "Anas");
+            string strWords = "";
+            strWords += "Welcome";
+            strWords += " " + "To" + " " + "Egypte";
+           // strWords -= "Egypte"; //Incorrect;
+            Console.WriteLine(strWords);
+        }
+    }
+}
