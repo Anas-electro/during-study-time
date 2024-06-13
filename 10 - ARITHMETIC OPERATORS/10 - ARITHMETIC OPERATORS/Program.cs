ec5c79972d41430f5085efd4ea81a87820c98275
Anas-Electro <anas.electro212@gmail.com>
2024-06-13 16:39:29 +0100

    C# ARITHMETIC OPERATORS

---
+++ 10 - ARITHMETIC OPERATORS/10 - ARITHMETIC OPERATORS/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___ARITHMETIC_OPERATORS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Operator +
+            //int nbr1 = 15;
+            //int nbr2 = 22;
+            //int res = nbr1 + nbr2;
+            //Console.WriteLine(res);
+
+
+            //// Operator -
+            //int nbr1 = 10;
+            //int nbr2 = 7;
+            //int res = nbr1 - nbr2;
+            //Console.WriteLine(res);
+
+            /// Operator *
+
+            //int nbr1 = 5;
+            //int nbr2 = 5;
+            //int res = nbr1 * nbr2;
+            //Console.WriteLine(res);
+
+            // Operator /
+
+            // int nbr1 = 10;
+            // int nbr2 = 2;
+            //// int nbr2 = 0; //Incorrect;
+            // int res = nbr1 / nbr2;
+            // Console.WriteLine(res);
+
+            // Operator MOD % باقي القسمة الصحيحة
+
+            //  int nbr1 = 10;
+            //  int nbr2 = 3;
+
+            //  int res = nbr1 % nbr2;
+            //  Console.WriteLine(res);
+            ///*  10 mod 3
+            // *  10 - 3 = 7;
+            // *  7 - 3 = 4;
+            // *  4 - 3 = 1;
+            // */
+
+            ///////////
+            double nbr1 = 22.6;
+            double nbr2 = 20.2;
+            //double res = nbr1 + nbr2;
+            Console.WriteLine(nbr1 + nbr2);
+            Console.WriteLine(nbr1 - nbr2);
+            Console.WriteLine(nbr1 * nbr2);
+            Console.WriteLine(nbr1 / nbr2);
+            Console.WriteLine(nbr1 % nbr2);
+
+
+
+
+
+        }
+    }
+}
