32b070da86f22c29a0ffe2251849afba545a178b
Anas-Electro <anas.electro212@gmail.com>
2024-06-14 17:11:15 +0100

    C# . NET DATA TYPES

---
+++ 12 - Dot NET DATA TYPES/Dot NET DATA TYPES/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_NET_DATA_TYPES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte nbr1 = 255; // من اساس اللغة
+            Byte nbr2 = 255; // مبنية بعد اللغة
+
+            sbyte nbr3;
+            SByte nrb4;
+
+            int nbr5;
+            Int32 nbr6;
+
+            uint nbr7;
+            UInt32 nbr8;
+
+            short nbr9;
+            Int16 nbr10;
+
+            ushort nbr11;
+            UInt16 nbr12;
+
+            long nbr13;
+            Int64 nbr14;
+
+            ulong nbr15;
+            UInt64 nbr16;
+
+            float nbr17;
+            Single nbr18;
+
+            double nbr19;
+            Double nbr20;
+
+            char c1;
+            Char c2;
+
+            bool b1;
+            Boolean b2;
+
+            object ob1;
+            Object ob2;
+
+            string str1;
+            String str2;
+
+            decimal d1;
+            Decimal d2;
+
+
+        }
+    }
+}
