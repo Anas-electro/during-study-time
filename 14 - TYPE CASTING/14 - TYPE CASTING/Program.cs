8ceaf769769dc9698771b1737e93c39a15f8b896
Anas-Electro <anas.electro212@gmail.com>
2024-06-16 18:02:55 +0100

    C# TYPE CASTING CAST

---
+++ 14 - TYPE CASTING/14 - TYPE CASTING/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14___TYPE_CASTING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double dblNum = 11.55;
+            //int intNum;
+            //intNum = (int)dblNum;
+
+            //// intNum = Convert.ToInt32(dblNum);
+            //Console.WriteLine(intNum);
+
+            object obj = "Hello";
+            string str = (string)obj;
+
+            Console.WriteLine(str);
+            
+        }
+    }
+}
