e357a00f01005fe14be957d1e8b75f0d0d19bceb
Anas-Electro <anas.electro212@gmail.com>
2024-06-19 17:24:32 +0100

    C# GET CHAR UNICODE USING CAST

---
+++ 15 - MyChar/15 - MyChar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15___MyChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int x = 77; //dec:77 = M
+            //char c = (char)x;
+
+            //Console.WriteLine(c);
+
+            //char c = 'R';
+            //int x = (int)c;
+
+            //Console.WriteLine(x);
+
+            Console.WriteLine((char)77);
+            Console.WriteLine((int)'M');
+         
+        }
+    }
+}
