550e780484bb2851a8cc5c03de0fb4c4c7b49cd1
Anas-Electro <anas.electro212@gmail.com>
2024-06-20 17:38:31 +0100

    PRE FIX & POST FIX WITH X PLUS PLUS

---
+++ 16 - PRE FIX and POST FIX/16 - PRE FIX and POST FIX/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16___PRE_FIX_and_POST_FIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5;
+            //x = x + 1;
+            //x += 1;
+            //x++;
+            //++x;
+
+            //x = x - 1;
+            //x -= 1;
+            //x--;
+            //--x;
+
+
+            // Console.WriteLine(x);
+
+            ////// Postfix + جاي بعد المتغير
+            ////x++;
+            //// Prefix قبل المتغير
+            //++x;
+
+            int x = 5;
+            // int y = x++;
+            int y = ++x;
+
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("x = " + x);
+
+
+        }
+    }
+}
