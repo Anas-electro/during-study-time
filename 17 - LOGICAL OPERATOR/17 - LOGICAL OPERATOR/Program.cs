abfaf0d17482088aee1b2fbd04587090a8db093c
Anas-Electro <anas.electro212@gmail.com>
2024-06-21 17:15:58 +0100

    C# LOGICAL OPERATOR

---
+++ 17 - LOGICAL OPERATOR/17 - LOGICAL OPERATOR/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17___LOGICAL_OPERATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ahmed AND Amr
+            // true  true = true;
+            // false false = false;
+            // false false = ture;
+            // false true = true;
+            // في هذه الحالة يجب تحقق من كل من الاسطر
+
+
+            //Ahmed OR Amr
+            // true  true = true;
+            // true  false = ture;
+            // true  true  = false;
+            // false false = false;
+
+            // NOT true = false;
+            // NOT false = true;
+            // NOT Ahmed = every body except ahmed
+            // NOT Amr  = every body except amr
+        }
+    }
+}
