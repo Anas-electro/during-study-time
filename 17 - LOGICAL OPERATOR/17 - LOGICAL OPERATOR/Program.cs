b238580f60e491bfafbe778f72fccdd7eb28acc1
Anas-Electro <anas.electro212@gmail.com>
2024-06-22 17:49:59 +0100

    C# LOGICAL OPERATOR

--- 17 - LOGICAL OPERATOR/17 - LOGICAL OPERATOR/Program.cs
+++ 17 - LOGICAL OPERATOR/17 - LOGICAL OPERATOR/Program.cs
@@ -28,6 +28,44 @@ namespace _17___LOGICAL_OPERATOR
             // NOT false = true;
             // NOT Ahmed = every body except ahmed
             // NOT Amr  = every body except amr
+
+
+            /////////AND
+            Console.WriteLine("AND Operator");
+            bool and1 = true && true;
+            bool and2 = true && false;
+            bool and3 = false && true;
+            bool and4 = false && false;
+            Console.WriteLine(and1);
+            Console.WriteLine(and2);
+            Console.WriteLine(and3);
+            Console.WriteLine(and4);
+
+            Console.WriteLine("=============");
+
+            ////////OR
+            Console.WriteLine("OR Operator");
+            bool or1 = true || true;
+            bool or2 = true || false;
+            bool or3 = false || true;
+            bool or4 = false || false;
+
+            Console.WriteLine(or1);
+            Console.WriteLine(or2);
+            Console.WriteLine(or3);
+            Console.WriteLine(or4);
+
+            Console.WriteLine("=============");
+
+            //////NOT
+            Console.WriteLine("NOT Operator");
+            bool not1 = !true;
+            bool not2 = !false;
+            Console.WriteLine(not1);
+            Console.WriteLine(not2);
+            Console.WriteLine("=============");
+
+
         }
     }
 }
