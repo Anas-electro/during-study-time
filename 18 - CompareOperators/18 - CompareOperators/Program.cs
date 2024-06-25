75192906a92c4ea283858327359d1cc4bf725c8e
Anas-Electro <anas.electro212@gmail.com>
2024-06-25 19:49:11 +0100

    C# COMPRESSION OPERATOR OR COMPARE

---
+++ 18 - CompareOperators/18 - CompareOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18___CompareOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbr1 = 10;
+            int nbr2 = 5;
+
+            bool b;
+            // b = nbr1 == nbr2;
+            // b = nbr1 > nbr2;
+            //b = nbr1 >= nbr2;
+            // b = nbr1 < nbr2;
+            // b = nbr1 <= nbr2;
+            b = nbr1 != nbr2;
+            Console.WriteLine(b);
+
+        }
+    }
+}
