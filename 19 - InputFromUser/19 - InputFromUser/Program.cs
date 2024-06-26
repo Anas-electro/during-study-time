c350755cbd2e5a1de42d03e549c88a2032547004
Anas-Electro <anas.electro212@gmail.com>
2024-06-26 17:05:09 +0100

    C# CONSOLE . READ LINE FOR USER INPUT

---
+++ 19 - InputFromUser/19 - InputFromUser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19___InputFromUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Console.WriteLine("Write any words: ");
+            //Console.Write("Write any word : ");
+            //string str = Console.ReadLine();
+            //Console.WriteLine("Input is : " + str);
+
+            //Console.Write("Enter Your Name: ");
+            //string strName = Console.ReadLine();
+            //Console.WriteLine("===========");
+            //Console.WriteLine("Welcome " + strName);
+            //Console.WriteLine("===========");
+
+            Console.Write("Enter Number1 = ");
+            string StrNbr1 = Console.ReadLine();
+            Console.Write("Enter Number2 = ");
+            string StrNbr2 = Console.ReadLine();
+            Console.Write("Enter Number = ");
+            string StrNbr3 = Console.ReadLine();
+            int nbr1 = int.Parse(StrNbr1);
+            int nbr2 = int.Parse(StrNbr2);
+            int nbr3 = Convert.ToInt32(StrNbr3);
+            int res = nbr1 + nbr2 + nbr3;
+            Console.WriteLine(res);
+
+        }
+    }
+}
---
+++ 19 - InputFromUser/19 - InputFromUser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("19 - InputFromUser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("19 - InputFromUser")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("41872f5e-b2c7-4016-9cbe-042cee9f9009")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
