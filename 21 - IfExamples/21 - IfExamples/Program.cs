f28c95c044580063bc5550b9d676fdecf3a3c453
Anas-Electro <anas.electro212@gmail.com>
2024-06-28 17:21:01 +0100

    C# IF STATEMENT EXAMPLES VISUAL STUDIO

---
+++ 21 - IfExamples/21 - IfExamples/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21___IfExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter Number 1: ");
+            //int nbr1 = int.Parse(Console.ReadLine());
+            //Console.Write("Enter Number 2: ");
+            //int nbr2 = Convert.ToInt32(Console.ReadLine());
+
+            //if (nbr1 > nbr2)
+            //{
+            //    Console.WriteLine("Number1 > Number2");
+            //}
+            //if (nbr2 > nbr1)
+            //{
+            //    Console.WriteLine("Number 2 > Number2");
+            //}
+            //if (nbr1 == nbr2)
+            //{
+            //    Console.WriteLine("Number1 = Number2");
+            //}
+
+            //    Console.Write("Enter Number1 : ");
+            //    int nbr1 = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter Number2 : ");
+            //    int nbr2 = Convert.ToInt32(Console.ReadLine());
+
+            //    if (nbr2 == 0)
+            //    {
+            //        Console.WriteLine("No / By Zero");
+            //        nbr2 = 1;
+            //    }
+            //    int res = nbr1 / nbr2;
+
+            //    Console.WriteLine(res);
+
+            //Console.WriteLine("Enter The Grade: ");
+            //int gr = int.Parse(Console.ReadLine());
+
+            //if (gr < 50)
+            //{
+            //    Console.WriteLine("Fail");
+            //}
+            //if (gr >= 50)
+            //{
+            //    Console.WriteLine("Pass");
+            //}
+
+
+            //Console.WriteLine("Enter Word1: ");
+            //string word1 = Console.ReadLine();
+
+            //Console.WriteLine("Enter Word2: ");
+            //string word2 = Console.ReadLine();
+
+            //if (word1 == word2)
+            //{
+            //    Console.WriteLine("Word1 = Word2");
+            //}
+            //if (word1 != word2)
+            //{
+            //    Console.WriteLine("Word1 <Not Equal> Word2");
+            //}
+
+
+            int x = 5;
+            int y = 10;
+
+            //if (x > y)
+            //    Console.WriteLine("X > Y");
+            //Console.WriteLine("X > Y");
+            if (x > y) Console.WriteLine("X > Y");
+
+        }
+
+    }
+}
