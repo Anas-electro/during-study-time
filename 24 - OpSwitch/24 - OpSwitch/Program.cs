9e75b826f8c169d2c4db681e4c09ca6c38dac891
Anas-Electro <anas.electro212@gmail.com>
2024-07-03 16:48:36 +0100

    C# SWITCH STATEMENT BASICS VISUAL STUDIO

---
+++ 24 - OpSwitch/24 - OpSwitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24___OpSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbr = 3;
+            switch(nbr)
+            {
+                case 1:
+                    Console.WriteLine(1);
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine(2);
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+                
+                    
+            }
+        }
+    }
+}
