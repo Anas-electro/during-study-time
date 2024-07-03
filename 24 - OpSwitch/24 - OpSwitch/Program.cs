1fe071409e7177205cd5594ca964ac61145e2aee
Anas-Electro <anas.electro212@gmail.com>
2024-07-03 17:26:35 +0100

    C# SWITCH STATEMENT BASICS VISUAL STUDIO

--- 24 - OpSwitch/24 - OpSwitch/Program.cs
+++ 24 - OpSwitch/24 - OpSwitch/Program.cs
@@ -10,25 +10,52 @@ namespace _24___OpSwitch
     {
         static void Main(string[] args)
         {
-            int nbr = 3;
-            switch(nbr)
+            //int nbr = 3;
+            //switch(nbr)
+            //{
+            //    case 1:
+            //        Console.WriteLine(1);
+            //        Console.WriteLine("One");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine(2);
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Three");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Other");
+            //        break;
+
+
+            // }
+
+            //char c = 'B';
+            //switch (c)
+            //{
+            //    case 'A':
+            //        Console.WriteLine("A");
+            //        break;
+            //    case 'B':
+            //        Console.WriteLine("B");
+            //        break;
+
+            int nbr = 2;
+            switch (nbr*2)
             {
-                case 1:
-                    Console.WriteLine(1);
-                    Console.WriteLine("One");
-                    break;
                 case 2:
-                    Console.WriteLine(2);
+                    Console.WriteLine(nbr + " * 2 = " + (nbr*2));
                     break;
-                case 3:
-                    Console.WriteLine("Three");
+                case 4:
+                    Console.WriteLine(nbr + " * 2 = " + (nbr * 2));
                     break;
-                default:
-                    Console.WriteLine("Other");
+                case 6:
+                    Console.WriteLine(nbr + " * 2 = " + (nbr * 2));
                     break;
-                
-                    
+           // }   
+
             }
+
         }
     }
 }
