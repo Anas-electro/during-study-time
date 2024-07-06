95deae88b6e9fabc6791500e1b658e33ca928779
Anas-Electro <anas.electro212@gmail.com>
2024-07-06 17:00:15 +0100

    C# PRINT CHARACTER USING FOR STATEMENT

---
+++ 028 - TestFor/028 - TestFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _028___TestFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Alphabet");
+            //Console.WriteLine("===============");
+            //for (char c = 'a'; c<= 'z'; c++)
+            //{
+            //    Console.WriteLine(c);
+            //   // Console.WriteLine((int)c);
+            //}
+
+            for (char c = 'z'; c >= 'a'; c--)
+            {
+                Console.WriteLine(c);
+              //  Console.WriteLine((int)c);
+            }
+        }
+    }
+}
