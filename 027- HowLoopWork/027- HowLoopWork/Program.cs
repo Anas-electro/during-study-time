0079a210323774c5bd97e4b2253f64d5710c6763
Anas-Electro <anas.electro212@gmail.com>
2024-07-06 11:33:07 +0100

    C# HOW WORK FOR STATEMENT VISUAL STUDIO

---
+++ 027- HowLoopWork/027- HowLoopWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _027__HowLoopWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x=1;x<=10;x++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("Sorry x not equal 10");
+        }
+    }
+}
