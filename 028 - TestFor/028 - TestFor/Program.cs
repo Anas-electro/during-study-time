f9ffe66a30ee54226ab53d5e408a7e9d48f310fc
Anas-Electro <anas.electro212@gmail.com>
2024-07-10 16:49:25 +0100

    C# MANY VAR IN FOR STATEMENT

--- 028 - TestFor/028 - TestFor/Program.cs
+++ 028 - TestFor/028 - TestFor/Program.cs
@@ -10,19 +10,33 @@ namespace _028___TestFor
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Alphabet");
-            //Console.WriteLine("===============");
-            //for (char c = 'a'; c<= 'z'; c++)
+            ////Console.WriteLine("Alphabet");
+            ////Console.WriteLine("===============");
+            ////for (char c = 'a'; c<= 'z'; c++)
+            ////{
+            ////    Console.WriteLine(c);
+            ////   // Console.WriteLine((int)c);
+            ////}
+
+            //for (char c = 'z'; c >= 'a'; c--)
             //{
             //    Console.WriteLine(c);
-            //   // Console.WriteLine((int)c);
+            //  //  Console.WriteLine((int)c);
             //}
 
-            for (char c = 'z'; c >= 'a'; c--)
-            {
-                Console.WriteLine(c);
-              //  Console.WriteLine((int)c);
-            }
+            //for (int x = 1, y = 10; x<=10;x++,y--)
+            //{
+            //    Console.WriteLine(x + ":" + y);
+            //   // Console.WriteLine("X = " + x);
+            //   // Console.WriteLine("Y = " + y);
+            //}
+
+
+            //for (int x = 1, y = 10, z = 100; x <= 10; x++, y += 10, z+=100)
+            //    Console.WriteLine(x + ":" + y + ":" + z);
+
+            for (int x = 1, y = 10, z = 100; x <= 10 && y <= 30; x++, y += 10, z += 100)
+                Console.WriteLine(x + ":" + y + ":" + z);
         }
     }
 }
