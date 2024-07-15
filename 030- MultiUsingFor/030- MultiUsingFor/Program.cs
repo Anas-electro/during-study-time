f0880cd4571c690c9943c0ffcbd58b86853bc987
Anas-Electro <anas.electro212@gmail.com>
2024-07-15 17:26:15 +0100

    C# MULTIPLICATION TABLE USING FOR STATEMENT

---
+++ 030- MultiUsingFor/030- MultiUsingFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _030__MultiUsingFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNum;
+            Console.WriteLine("Enter The Number: ");
+            baseNum = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(baseNum + " x " + i + " = " + baseNum * i);
+            }
+        }
+    }
+}
