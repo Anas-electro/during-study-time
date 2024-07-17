63043d15d74d919878d1fbbba42c537fb9482d9c
Anas-Electro <anas.electro212@gmail.com>
2024-07-17 12:43:05 +0100

    C# EXAMPLES NESTED FOR STATEMENT

---
+++ 032 - NestedForExamples/032 - NestedForExamples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _032___NestedForExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (int dept = 1; dept <= 4; dept++)
+            //{
+            //   /// Console.WriteLine("--------------------");
+            //    Console.WriteLine("Departement : " + dept);
+            //    Console.WriteLine("--------------------");
+            //    for (int emp = 1; emp <= 3; emp++)
+            //    {
+            //        Console.WriteLine("  Employee : " + emp);
+            //    }
+            //    Console.WriteLine("--------------------");
+            //}
+
+            for (int com = 1; com <= 2; com++)
+            {
+                Console.WriteLine("Company " + com);
+                Console.WriteLine("_________________");
+                for(int b = 1; b<=2;b++)
+                {
+                    Console.WriteLine("   Building " + b);
+                    Console.WriteLine("   -----------");
+                    for (int f = 1; f<=3; f++)
+                    {
+                        Console.WriteLine("      Floor" + f);
+
+                        for (int r = 1; r<=2; r++)
+                        {
+                            Console.WriteLine("           Room " + r);
+                        }
+                    }
+                    Console.WriteLine("------------------");
+                }
+            }
+
+        }
+    }
+}
