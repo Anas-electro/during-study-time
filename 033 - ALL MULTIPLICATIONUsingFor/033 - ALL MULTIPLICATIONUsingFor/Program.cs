78be9c16dd3448e92127f2f323aa0e83aa09c5e4
Anas-Electro <anas.electro212@gmail.com>
2024-07-18 17:37:36 +0100

    ALL MULTIPLICATION TABLE USING FOR STATEMENT

---
+++ 033 - ALL MULTIPLICATIONUsingFor/033 - ALL MULTIPLICATIONUsingFor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _033___ALL_MULTIPLICATIONUsingFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int baseNum = 1; baseNum<=10; baseNum++)
+            {
+                Console.WriteLine("Table " + baseNum);
+                Console.WriteLine("-----------");
+                for (int num = 1; num<=10;num++ )
+                {
+                    Console.WriteLine("  " + baseNum + " x " + num + " = " + baseNum * num);
+                }
+                Console.WriteLine("==========");
+            }
+        }
+    }
+}
