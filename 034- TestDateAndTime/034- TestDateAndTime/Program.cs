c291c9d0562a5bc9973855d22055e1444670189f
Anas-Electro <anas.electro212@gmail.com>
2024-07-19 10:39:48 +0100

    C# PRINT DATE AND TIME VISUAL STUDIO

---
+++ 034- TestDateAndTime/034- TestDateAndTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _034__TestDateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string strDt;
+            //strDt = DateTime.Now.ToString();
+            //Console.WriteLine(strDt);
+
+            // Console.WriteLine(DateTime.Now);
+
+            DateTime dt;
+            dt = Convert.ToDateTime("01/01/2000 00:00:11");
+            Console.WriteLine(dt);
+
+        }
+    }
+}
