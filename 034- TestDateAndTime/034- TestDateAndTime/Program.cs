3913b88a68d384f1b44febef5b0aa2fed792c256
Anas-Electro <anas.electro212@gmail.com>
2024-07-20 10:23:02 +0100

    PRINT CUSTOMIZATION DATE AND TIME

--- 034- TestDateAndTime/034- TestDateAndTime/Program.cs
+++ 034- TestDateAndTime/034- TestDateAndTime/Program.cs
@@ -16,9 +16,36 @@ namespace _034__TestDateAndTime
 
             // Console.WriteLine(DateTime.Now);
 
-            DateTime dt;
-            dt = Convert.ToDateTime("01/01/2000 00:00:11");
-            Console.WriteLine(dt);
+            //DateTime dt;
+            //dt = Convert.ToDateTime("01/01/2000 00:00:11");
+            //Console.WriteLine(dt);
+
+            ///////////////////////////
+
+            // string strDT;
+            // strDT = DateTime.Now.ToString("d");
+            //strDT = DateTime.Now.ToString("D");
+            //strDT = DateTime.Now.ToString("t");
+            //strDT = DateTime.Now.ToString("T");
+            //strDT = DateTime.Now.ToString("dd");
+            //  strDT = DateTime.Now.ToString("MM");
+            //strDT = DateTime.Now.ToString("yyyy");
+            //strDT = DateTime.Now.ToString("yy");
+            //strDT = DateTime.Now.ToString("M");
+            //strDT = DateTime.Now.ToString("y");
+            // strDT = DateTime.Now.ToString("dd/MM/yyyy");
+            //strDT = DateTime.Now.ToString("dd/MM/yy");
+            //strDT = DateTime.Now.ToString("dd-MM-yyyy");
+            //strDT = DateTime.Now.ToString("d/M/yyyy");
+            //strDT = DateTime.Now.ToString("hh");
+            //strDT = DateTime.Now.ToString("mm");
+            //strDT = DateTime.Now.ToString("ss");
+            //strDT = DateTime.Now.ToString("tt"); //pm/am
+            // strDT = DateTime.Now.ToString("hh:mm:ss tt");
+            //strDT = DateTime.Now.ToString("dd/MM/yyyy - hh:mm:ss tt");
+            // Console.WriteLine(strDT)  ;
+
+            Console.WriteLine(DateTime.Now.AddYears(5).AddMonths(1).AddDays(3).AddHours(1).AddMinutes(10).ToString("dd-MM-yyyy  - hh:mm:ss tt"));
 
         }
     }
