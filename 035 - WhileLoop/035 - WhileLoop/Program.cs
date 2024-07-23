1ea14decb35ba394c46fa98fadd3dc8194ec7e97
Anas-Electro <anas.electro212@gmail.com>
2024-07-23 16:52:54 +0100

    C# LOOP USING WHILE STATEMENT VISUAL STUDIO

---
+++ 035 - WhileLoop/035 - WhileLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _035___WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///// لوووب لا نهائية
+            //while(true)
+            //{
+            //    Console.Write(101);
+            //    Console.Write(102);
+            //    Console.Write(103);
+            //}
+
+
+            //int x = 1;
+            //while(x <= 7)
+            //{
+            //    Console.WriteLine(x);
+            //     x++;
+            //   // x--;
+            //}
+
+            int x = 10;
+            while (x>=1)
+            {
+                Console.WriteLine(x);
+                x--;
+            }
+
+        }
+    }
+}
