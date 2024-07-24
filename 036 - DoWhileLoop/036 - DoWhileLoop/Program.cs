2a747bc2e0613d370890fd81304444828851f815
Anas-Electro <anas.electro212@gmail.com>
2024-07-24 18:44:57 +0100

    C# DO WHILE LOOP STATEMENT VISUAL STUDIO

---
+++ 036 - DoWhileLoop/036 - DoWhileLoop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _036___DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do
+            //{
+            //    Console.WriteLine("true");
+            //} while (true);
+
+            //int x = 1;
+            //do
+            //{
+            //    Console.WriteLine("Loop Nbr:" + x);
+            //    x++;
+            //} while (x <= 10);
+
+            //int x = 10;
+            //do
+            //{
+            //    Console.WriteLine("Loop Nbr: " + x);
+            //    x--;
+            //} while (x >= 1);
+
+            //for(int x = 1; false; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            //int x = 1;
+            //while (false)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (false);
+
+        }
+    }
+}
