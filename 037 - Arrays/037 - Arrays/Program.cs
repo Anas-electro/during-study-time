e3d235fa6be2bbf79b548b5cc1ff8d49a6154f8c
Anas-Electro <anas.electro212@gmail.com>
2024-07-26 16:55:46 +0100

    C# START WITH ARRAYS VISUAL STUDIO

---
+++ 037 - Arrays/037 - Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _037___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+//            Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+//To declare an array, define the variable type with square brackets:
+
+//string[] cars;
+
+//            We have now declared a variable that holds an array of strings.
+
+//To insert values to it, we can use an array literal -place the values in a comma-separated list, inside curly braces:
+
+//            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+//            To create an array of integers, you could write:
+
+//int[] myNum = { 10, 20, 30, 40 };
+
+        }
+    }
+}
