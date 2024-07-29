4d264a4f04a0b6d34217e76f1cf6124d29aec0fd
Anas-Electro <anas.electro212@gmail.com>
2024-07-29 16:56:53 +0100

    سي شارب C# ARRAY LENGTH AND CHAR ARRAY WITH STRING

--- 038 - ArraysExamples/038 - ArraysExamples/Program.cs
+++ 038 - ArraysExamples/038 - ArraysExamples/Program.cs
@@ -37,24 +37,53 @@ namespace _038___ArraysExamples
             //Console.WriteLine(nbrs[1]);
             //Console.WriteLine(nbrs[2]);
 
-            object[] data = new object[7];
+            //object[] data = new object[7];
 
-            data[0] = "ABC";
-            data[1] = 1;
-            data[2] = true;
-            data[3] = 'M';
-            data[4] = 3.14444;
-            data[5] = 12345678901034506;
-            data[6] = false;
+            //data[0] = "ABC";
+            //data[1] = 1;
+            //data[2] = true;
+            //data[3] = 'M';
+            //data[4] = 3.14444;
+            //data[5] = 12345678901034506;
+            //data[6] = false;
 
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
+            //Console.WriteLine(data[0]);
+            //Console.WriteLine(data[1]);
+            //Console.WriteLine(data[2]);
+            //Console.WriteLine(data[3]);
+            //Console.WriteLine(data[4]);
+            //Console.WriteLine(data[5]);
+            //Console.WriteLine(data[6]);
 
+            //Console.WriteLine("Enter The Count");
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //string[] str = new string[c];
+            //str[0] = "Ahmed";
+            //str[str.Length - 1] = "Amr";
+            //int arrayCount = str.Length;
+
+            //Console.WriteLine(arrayCount);
+            //Console.WriteLine(str[str.Length - 1]);
+
+            //char[] cName = new char[5];
+            //cName[0] = 'A';
+            //cName[1] = 'H';
+            //cName[2] = 'M';
+            //cName[3] = 'E';
+            //cName[4] = 'D';
+
+            //// Console.WriteLine(cName[0].ToString() + cName[1] + cName[2]);
+
+            //// string strName = cName.ToString();
+            //string strName = new string(cName);
+            //Console.WriteLine(strName);
+
+            string strName = "AHMED";
+            char[] cName;
+            cName = strName.ToCharArray();
+            Console.WriteLine(cName);
+        
 
 
         }
