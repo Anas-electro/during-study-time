a4c7e3b3c37b7f9aca173aa6a6e7a6657e3c81cb
Anas-Electro <anas.electro212@gmail.com>
2024-07-31 16:49:29 +0100

    C# FOREACH LOOP VISUAL STUDIO

---
+++ 040 - ForEachLoop/040 - ForEachLoop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _040___ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  string[] strNames = new string[3];
+            //   string strNames = "";
+
+
+            // int[] intS = new int[3];
+            // int i = 0;
+
+            //string[] strNames = {"Amr", "Ali", "Tamer" };
+
+            ////for (int index=0;index<strName.Length;index++)
+            ////{
+            ////    Console.WriteLine(strNames[index]);
+            ////}
+
+            //foreach(string strName in strNames)
+            //{
+            //    Console.WriteLine(strName);
+            //}
+
+            string strName = "Abdollah";
+            //char[] cName = strName.ToCharArray();
+            //foreach(char c in cName)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            foreach(char c in strName)
+            {
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
