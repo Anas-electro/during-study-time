24ff7e8eec8d8474905ad00ec535b5018861ad51
Anas-Electro <anas.electro212@gmail.com>
2024-08-01 15:46:22 +0100

    C# TRACING PROGRAM VISUAL STUDIO

---
+++ 041 - TracingProgram/041 - TracingProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _041___TracingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strName;
+            strName = "Omar";
+            Console.WriteLine(strName);
+
+            string[] strNames = { "Ahmed", "Amr", "Abdel", "Ehab" };
+            for(int index = 0;index<4;index++)
+            {
+                Console.WriteLine(strNames[index]);
+            }
+        }
+    }
+}
