ac80706a7b94764d688e689efb486e3fa0903c9c
Anas-Electro <anas.electro212@gmail.com>
2024-08-02 16:34:13 +0100

    C# GENERATE RANDOM NUMBERS VISUAL STUDIO

---
+++ 042 - GenerateRandom/042 - GenerateRandom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _042___GenerateRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            // int rNum = rnd.Next();
+            int rNum;
+            //  rNum = rnd.Next(1,20);
+            //rNum = rnd.Next(1, 13);
+            //DateTime dt = Convert.ToDateTime(rNum + "/" + rNum + "/2000");
+
+            //Console.WriteLine(dt.ToString("MMMM"));
+
+
+            // Console.WriteLine(rNum);
+
+            rNum = rnd.Next(1, 8);
+            Console.WriteLine(rNum);
+        }
+    }
+}
