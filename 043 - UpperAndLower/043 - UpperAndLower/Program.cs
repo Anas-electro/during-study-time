b694094ad40edf25f3081070a20dc93fd83eb11a
Anas-Electro <anas.electro212@gmail.com>
2024-08-03 16:27:55 +0100

    C# UPPER CASE AND LOWER CASE CHARACTERS

---
+++ 043 - UpperAndLower/043 - UpperAndLower/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _043___UpperAndLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "abcdefgh";
+            //string strUP = str.ToUpper();
+            //string strLow = str.ToLower();
+            //Console.WriteLine(str);
+            //Console.WriteLine(strUP);
+            //Console.WriteLine(strLow);
+
+            string strName = "Ahmed";
+            string strName2 = "";
+            char[] cName = strName.ToCharArray();
+            for (int index=0;index <5;index++)
+            {
+                if (index == 3)
+                {
+                strName2 += cName[index].ToString().ToUpper();
+                }
+                else
+                {
+                    strName2 += cName[index];
+                }
+            }
+            Console.WriteLine(strName2);
+        }
+    }
+}
