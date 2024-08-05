d7d0819462dccd4609e4e3be4ff8c5c7e4ba92f0
Anas-Electro <anas.electro212@gmail.com>
2024-08-05 17:25:10 +0100

    C# LENGTH CHARACTERS OF STRING

---
+++ 044- LenghtChar/044- LenghtChar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _044__LenghtChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name and Max Digits is 10 : ");
+            string strName = Console.ReadLine();
+            if (strName.Length > 10)
+            {
+                Console.WriteLine("INVALID NAME");
+            }
+            else
+            {
+                Console.WriteLine("Hello " + strName);
+            }
+        }
+    }
+}
