7de4fcb24b6c1785a9eb4016fdbf0d851b43456b
Anas-Electro <anas.electro212@gmail.com>
2024-08-06 16:40:02 +0100

    C# STRING DOT FORMAT VISUAL STUDIO

---
+++ 045 - StringDotFormat/045 - StringDotFormat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _045___StringDotFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string strName = "Ahmed";
+            string strJobs = "Programmer";
+            string strPhone = "0722229805";
+
+            //string strData = "Name: " + strName
+            //               + "\nJob: " + strJobs
+            //               + "\nPhone: " + strPhone;
+
+            string strData = string.Format("Name: {0}\nJob: {1}\nPhone: {2}",strName,strJobs,strPhone);
+            Console.WriteLine(strData); 
+        }
+    }
+}
