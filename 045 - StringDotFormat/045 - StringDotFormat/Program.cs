f68d2794f75cb771d14f7e023466992c86f958c5
Anas-Electro <anas.electro212@gmail.com>
2024-08-07 16:58:03 +0100

    سي شارب C# ADVANCED STRING DOT FORMAT AND CONSOLE

--- 045 - StringDotFormat/045 - StringDotFormat/Program.cs
+++ 045 - StringDotFormat/045 - StringDotFormat/Program.cs
@@ -11,16 +11,45 @@ namespace _045___StringDotFormat
         static void Main(string[] args)
         {
 
-            string strName = "Ahmed";
-            string strJobs = "Programmer";
-            string strPhone = "0722229805";
+            //string strName = "Ahmed";
+            //string strJobs = "Programmer";
+            //string strPhone = "0722229805";
 
-            //string strData = "Name: " + strName
-            //               + "\nJob: " + strJobs
-            //               + "\nPhone: " + strPhone;
+            ////string strData = "Name: " + strName
+            ////               + "\nJob: " + strJobs
+            ////               + "\nPhone: " + strPhone;
 
-            string strData = string.Format("Name: {0}\nJob: {1}\nPhone: {2}",strName,strJobs,strPhone);
-            Console.WriteLine(strData); 
+            //string strData = string.Format("Name: {0}\nJob: {1}\nPhone: {2}",strName,strJobs,strPhone);
+            //Console.WriteLine(strData); 
+
+
+            //int invoiceNum = 1;
+            //string strInvoice = string.Format("{0:00000}", invoiceNum);
+            //Console.WriteLine(strInvoice);
+
+            //int[] InvoiceNumbers = { 1, 50, 155, 6000, 123, 900, 1000, 999 };
+            //foreach(int index in InvoiceNumbers)
+            //{
+            //    Console.WriteLine(string.Format("{0:000000}",index));
+            //}
+
+            //int price = 150;
+
+            //Console.WriteLine(string.Format("{0:c6}", price));
+
+            //int salary = 7000;
+            //string strSal = string.Format("{0:f2}", salary);
+            //Console.WriteLine(strSal);
+
+            //DateTime dt = Convert.ToDateTime("01 / 01 / 2000 03:15 pm");
+            //string myDate = string.Format("{0:dd/MM/yyyy}",dt);
+            //Console.WriteLine(myDate);
+
+            //int num = 10550000;
+            //Console.WriteLine(string.Format("{0:X}", num));
+
+            string strName = "Adel";
+            Console.WriteLine("Name: {0}", strName);
         }
     }
 }
