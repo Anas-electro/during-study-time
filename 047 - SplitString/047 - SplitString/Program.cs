bca737322d06e28e2b94a16ac706ca6c93727100
Anas-Electro <anas.electro212@gmail.com>
2024-08-09 16:54:07 +0100

    سي شارب C# SPLIT STRING VISUAL STUDIO

---
+++ 047 - SplitString/047 - SplitString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _047___SplitString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strText;
+            strText = "Welcome To Egypt";
+
+            string[] AllText = strText.Split(' ');
+
+            foreach(string s in AllText)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
